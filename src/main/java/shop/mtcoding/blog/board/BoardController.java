9ddb2c12efe1444818a97cb0f2c633abc551c0a0
getinthere <getinthere@naver.com>
2024-03-12 16:12:42 +0900

    블로그 익명게시판 완료상태에서 새로 시작

---
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -0,0 +1,62 @@
+package shop.mtcoding.blog.board;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class BoardController {
+
+    private final BoardNativeRepository boardNativeRepository;
+
+    @PostMapping("/board/{id}/update")
+    public String update(@PathVariable Integer id, String title, String content, String username){
+        boardNativeRepository.updateById(id, title, content, username);
+        return "redirect:/board/"+id;
+    }
+
+    @GetMapping("/board/{id}/update-form")
+    public String updateForm(@PathVariable Integer id, HttpServletRequest request){
+        Board board = boardNativeRepository.findById(id);
+        request.setAttribute("board", board);
+
+        return "board/update-form";
+    }
+
+    @PostMapping("/board/{id}/delete")
+    public String delete(@PathVariable Integer id){
+        boardNativeRepository.deleteById(id);
+        return "redirect:/";
+    }
+
+    @PostMapping("/board/save")
+    public String save(String title, String content, String username){
+        boardNativeRepository.save(title, content, username);
+        return "redirect:/";
+    }
+
+    @GetMapping("/" )
+    public String index(HttpServletRequest request) {
+        List<Board> boardList = boardNativeRepository.findAll();
+        request.setAttribute("boardList", boardList);
+        return "index";
+    }
+
+    @GetMapping("/board/save-form")
+    public String saveForm() {
+        return "board/save-form";
+    }
+
+    @GetMapping("/board/{id}")
+    public String detail(@PathVariable Integer id, HttpServletRequest request) {
+        Board board = boardNativeRepository.findById(id);
+        request.setAttribute("board", board);
+        return "board/detail";
+    }
+}
