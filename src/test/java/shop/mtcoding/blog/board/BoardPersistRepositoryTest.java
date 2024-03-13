34e508098d4cd01699d57de31436ce7eadf462fd
getinthere <getinthere@naver.com>
2024-03-13 10:09:35 +0900

    게시글 상세보기 완료

--- src/test/java/shop/mtcoding/blog/board/BoardPersistRepositoryTest.java
+++ src/test/java/shop/mtcoding/blog/board/BoardPersistRepositoryTest.java
@@ -17,6 +17,21 @@ public class BoardPersistRepositoryTest {
     private BoardPersistRepository boardPersistRepository;
 
     @Test
+    public void findById_test() {
+        // given
+        int id = 1;
+
+        // when
+        Board board = boardPersistRepository.findById(id);
+
+        System.out.println("findById_test "+board);
+
+        // then
+        assertThat(board.getTitle()).isEqualTo("제목1");
+        assertThat(board.getContent()).isEqualTo("내용1");
+    }
+
+    @Test
     public void findAll_test() {
         // given
 
